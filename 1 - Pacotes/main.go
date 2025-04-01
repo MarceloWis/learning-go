0ed5141331e1848dabf99fb2245c657d1892b5ef
Marcelo Wischniowski <=>
2025-04-01 16:05:38 -0300

    Aula sobre pacotes externos

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Hello, world! Main")
+
 	auxiliar.Escrever()
 	auxiliar.Escrever2()
+	err := checkmail.ValidateFormat("marcelowiscgmail.com")
+	fmt.Println(err)
 }
